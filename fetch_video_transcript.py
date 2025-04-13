7626d83a952ccbb8db5bf1810d5ddd4f26b31bb6
Dhanush A M <139379331+AlokDhanush@users.noreply.github.com>
2025-04-13 23:09:05 +0530

    Update fetch_video_transcript.py

--- fetch_video_transcript.py
+++ fetch_video_transcript.py
@@ -3,5 +3,9 @@ from youtube_transcript_api import YouTubeTranscriptApi
 def get_transcript(video_url):
     v_ind = video_url.index("v=") # Extract video ID
     video_id = video_url[v_ind+2:v_ind+13]
-    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages = ['en', 'es', 'fr', 'hi'])
+    try:
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages = ['en', 'es', 'fr', 'hi'])
+    except Exception:
+        return ""
+        
     return " ".join([t['text'] for t in transcript])
